c9d495d6c7b56cd8f733a60a649f6b06ccc263a5
Mayuresh Satao <msatao75@gmail.com>
2025-11-15 15:24:38 -0500

    removing tsx page

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -47,15 +47,6 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
 }
 
 
-// Validate ../../../app/app/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/app">> = Specific
-  const handler = {} as typeof import("../../../app/app/page.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../../app/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
--- app/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// experiment type
-// number of reactions
-// how many experiements 
-// how many samples per experiment
-// patient number
-// preferred supplier
